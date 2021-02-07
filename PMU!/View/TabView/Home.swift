549f02dbc0e50df7287bfebc734506b616e23280
melwayzx <mewiiz.h@gmail.com>
2021-02-08 01:31:30 +0700

    Import view

--- PMU!/View/TabView/Home.swift
+++ PMU!/View/TabView/Home.swift
@@ -8,13 +8,44 @@
 import SwiftUI
 
 struct Home: View {
+    @State var name: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            VStack{
+                VStack{
+                    Text("PICK MEAL UP")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(red: 0.00, green: 0.13, blue: 0.25)).padding()
+                    
+                    HStack{
+                        Image(systemName: "magnifyingglass")
+                        TextField("โปรดกรอกชื่อร้านอาหาร", text: $name)
+                            .padding(.leading, 2.0)
+                            .font(.custom("Sukhumvit Set", size: 14 ))
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .frame(width: 304, height: 36, alignment: .center)
+                    }.padding(.bottom, 10)
+                }
+                ScrollView(showsIndicators: false) {
+                    VStack{
+                        CategoryList().padding(20)
+                        PopularList().padding(.leading,20)
+                        VStack(alignment: .leading){
+                            Text("ร้านอาหารใกล้คุณ").font(.custom("Sukhumvit Set",size: 18, relativeTo: .headline)).foregroundColor(Color(red: 0.00, green: 0.13, blue: 0.25)).bold().padding(.top, 5)
+                            RestaurantList()
+                            
+                        }
+                    }
+                    
+                }
+            }
+            .navigationBarHidden(true)
+        }
     }
-}
-
-struct Home_Previews: PreviewProvider {
-    static var previews: some View {
-        Home()
+    
+    struct Home_Previews: PreviewProvider {
+        static var previews: some View {
+            Home()
+        }
     }
 }
