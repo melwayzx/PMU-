682b3d2e54b988949ccfffe4c823d48b6970b7e5
melwayzx <mewiiz.h@gmail.com>
2021-02-07 21:39:11 +0700

    Initial Commit

---
+++ PMU!/PMU_App.swift
@@ -0,0 +1,17 @@
+//
+//  PMU_App.swift
+//  PMU!
+//
+//  Created by Melwayz's  on 7/2/21.
+//
+
+import SwiftUI
+
+@main
+struct PMU_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
