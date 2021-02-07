9b1492351a7e317f12b0cf87bda674359d36bb10
melwayzx <mewiiz.h@gmail.com>
2021-02-08 00:54:27 +0700

    Create popular restaurant list

---
+++ PMU!/View/Popular/PopularList.swift
@@ -0,0 +1,35 @@
+//
+//  PopularList.swift
+//  PMU!
+//
+//  Created by Melwayz's  on 8/2/21.
+//
+
+import SwiftUI
+
+struct PopularList: View {
+    let restaurant = Restaurants.all()
+
+    var body: some View {
+        
+        VStack(alignment: .leading)  {
+            Text("ร้านอาหารยอดนิยม").font(.custom("Sukhumvit Set",size: 18, relativeTo: .headline)).foregroundColor(Color(red: 0.00, green: 0.13, blue: 0.25)).bold().padding(.leading, 15)
+                .padding(.top, 5)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(self.restaurant, id: \.name){item in
+                        PopularItem(item : item)
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+struct PopularList_Previews: PreviewProvider {
+    static var previews: some View {
+        PopularList()
+    }
+}
