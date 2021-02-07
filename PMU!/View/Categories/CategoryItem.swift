01c3cb692652cdbf929c4baff6f87f8ca1d207d7
melwayzx <mewiiz.h@gmail.com>
2021-02-07 22:59:37 +0700

    Create category item

--- PMU!/View/Categories/CategoryItem.swift
+++ PMU!/View/Categories/CategoryItem.swift
@@ -8,25 +8,13 @@
 import SwiftUI
 
 struct CategoryItem: View {
-    
-    var item : Types
-    
     var body: some View {
-        VStack(alignment: .center)
-        {
-            Image("item.image").resizable()
-                .frame(width: 80,height: 80)
-                .cornerRadius(10)
-                .overlay(RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color(red: 0.855, green: 0.855, blue: 0.855, opacity: 0.5), lineWidth: 0.5))
-            
-            Text(item.name).fontWeight(.semibold).font(.custom("Sukhumvit Set", size: 14)).foregroundColor(Color(red: 0.00, green: 0.13, blue: 0.25))
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 struct CategoryItem_Previews: PreviewProvider {
     static var previews: some View {
-        CategoryItem(item : Types.all()[0])
+        CategoryItem()
     }
 }
