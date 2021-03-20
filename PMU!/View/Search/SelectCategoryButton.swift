a690ef58f46ae918382108a9ca920dc6adaa0353
melwayzx <mewiiz.h@gmail.com>
2021-03-21 02:02:44 +0700

    Create select category button

---
+++ PMU!/View/Search/SelectCategoryButton.swift
@@ -0,0 +1,28 @@
+//
+//  SelectCategoryButton.swift
+//  PMU!
+//
+//  Created by Melwayz's  on 1/3/21.
+//
+
+import SwiftUI
+
+struct SelectCategoryButton: View {
+    
+    var item : Categories
+    let didSelect : [Int]
+    var body: some View {
+        Text(item.name)
+            .fontWeight(.bold)
+            .font(.custom("Sukhumvit Set", size: 14))
+            .fontWeight(.semibold)
+            .frame(width: 170, height: 40)
+            .background(didSelect.contains(item.id) ? Color(red: 0.00, green: 0.13, blue: 0.25) : Color.white)
+            .cornerRadius(40)
+            .foregroundColor(didSelect.contains(item.id) ?  Color.white : Color(red: 0.682, green: 0.702, blue: 0.745) )
+            .overlay(
+                RoundedRectangle(cornerRadius: 40)
+                    .stroke(didSelect.contains(item.id) ?  Color(red: 0.00, green: 0.13, blue: 0.25) : Color(red: 0.682, green: 0.702, blue: 0.745) , lineWidth: 1  )
+            )
+    }
+}
