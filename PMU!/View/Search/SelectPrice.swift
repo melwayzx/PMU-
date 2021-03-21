222a0ed884ab2dfa7f822f368adb0fa074e5c388
melwayzx <mewiiz.h@gmail.com>
2021-03-21 19:24:27 +0700

    Fix bug

--- PMU!/View/Search/SelectPrice.swift
+++ PMU!/View/Search/SelectPrice.swift
@@ -10,19 +10,20 @@ import SwiftUI
 struct SelectPrice: View {
     
     var item : PriceChoices
-    var didSelect : Int
+    var didSelect : String
+    
     var body: some View {
         VStack{
             Text(item.title).font(.custom("Sukhumvit Set", size: 16)).fontWeight(.bold)
 //            Text(item.subtitle).font(.custom("Sukhumvit Set", size: 14))
         }
         .frame(width: 60, height: 40)
-        .background(didSelect == item.id ? Color(red: 0.00, green: 0.13, blue: 0.25) : Color.white)
+        .background(didSelect == item.title ? Color(red: 0.00, green: 0.13, blue: 0.25) : Color.white)
         .cornerRadius(20)
-        .foregroundColor(didSelect == item.id  ?  Color.white : Color(red: 0.682, green: 0.702, blue: 0.745) )
+        .foregroundColor(didSelect == item.title  ?  Color.white : Color(red: 0.682, green: 0.702, blue: 0.745) )
         .overlay(
             RoundedRectangle(cornerRadius: 20)
-                .stroke(didSelect == item.id  ?  Color(red: 0.00, green: 0.13, blue: 0.25) : Color(red: 0.682, green: 0.702, blue: 0.745) , lineWidth: 1  )
+                .stroke(didSelect == item.title  ?  Color(red: 0.00, green: 0.13, blue: 0.25) : Color(red: 0.682, green: 0.702, blue: 0.745) , lineWidth: 1  )
         )
     }
 }
