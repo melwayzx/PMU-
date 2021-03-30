49420e706e14e71ab9a8646058f42dd7cee4d034
melwayzx <mewiiz.h@gmail.com>
2021-03-30 17:58:39 +0700

    Create filter sort by options

--- PMU!/View/Search/SearchResult.swift
+++ PMU!/View/Search/SearchResult.swift
@@ -8,85 +8,91 @@
 import SwiftUI
 
 struct SearchResult: View {
-    
-    @Binding var listSorted : [Restaurant]
+    @Binding var text : String
+    @Binding var result : [Restaurant]
     var restaurantList : [Restaurant]
     @Binding var selectedCategory : [Int]
-    @Binding var selectedPrice : String
+    @Binding var selectedPrice : [String]
     @Binding var selectDistance : Double
+    @Binding var selectOption : [String]
+    @Binding var selectOpenNow : Bool
     @State var didSelectSort : Int  = 0
     
-    let filterAll : [Filter] = [Filter(id : 0 , title : "ระยะทางใกล้ฉันที่สุด"),Filter(id : 1 , title : "ราคาต่ำสุด"),Filter(id : 2 , title : "ราคาสูงสุด"),Filter(id : 3 , title : "คะแนนรีวิว"), ]
+    
+    let filterAll : [Filter] = [Filter(id : 0 , title : "ระยะทางใกล้ฉันที่สุด"),Filter(id : 1 , title : "ราคาต่ำสุด"),Filter(id : 2 , title : "คะแนนรีวิว"), ]
     
     var body: some View {
-        VStack(alignment: .leading ,  spacing: 10){
-            
-            HStack{
-                Text("เรียงผลการค้นหา :").foregroundColor(.gray)
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(alignment: .top, spacing: 5) {
-                        ForEach(filterAll){item in
-                            
-                            Button(action: {
-                                self.didSelectSort = item.id
-                                //                                print(didSelectSort)
-                            }){
-                                Text(item.title)
-                                    .frame(height: 30)
-                                    .padding(.leading,10)
-                                    .padding(.trailing, 10)
-                                    .background(didSelectSort == item.id ? Color(red: 0.00, green: 0.13, blue: 0.25) : Color.white)
-                                    .cornerRadius(40)
-                                    .foregroundColor(didSelectSort == item.id ? Color.white : Color(red: 0.682, green: 0.702, blue: 0.745) )
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 40)
-                                            .stroke(didSelectSort == item.id ?  Color(red: 0.00, green: 0.13, blue: 0.25) : Color(red: 0.898, green: 0.898, blue: 0.898, opacity: 0.22) , lineWidth: 1  )
-                                    )
+        VStack{
+            Group{
+                HStack{
+                    Text("เรียงผลการค้นหา :").foregroundColor(.gray)
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(alignment: .top, spacing: 5) {
+                            ForEach(filterAll){item in
+                                
+                                Button(action: {
+                                    self.didSelectSort = item.id
+                                    //                                print(didSelectSort)
+                                }){
+                                    Text(item.title)
+                                        .frame(height: 30)
+                                        .padding(.leading,10)
+                                        .padding(.trailing, 10)
+                                        .background(didSelectSort == item.id ? Color(red: 0.00, green: 0.13, blue: 0.25) : Color.white)
+                                        .cornerRadius(40)
+                                        .foregroundColor(didSelectSort == item.id ? Color.white : Color(red: 0.682, green: 0.702, blue: 0.745) )
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 40)
+                                                .stroke(didSelectSort == item.id ?  Color(red: 0.00, green: 0.13, blue: 0.25) : Color(red: 0.898, green: 0.898, blue: 0.898, opacity: 0.22) , lineWidth: 1  )
+                                        )
+                                }
                             }
+                            
+                            
                         }
                         
-                        
                     }
+                }.padding(.leading)
+            }
+            
+            
+            
+            if(result.count != 0){
+                ScrollView(showsIndicators: false) {
+                    Text("พบผลลัพธ์ทั้งหมด \(result.count) รายการ").foregroundColor(Color(red: 0.00, green: 0.13, blue: 0.25)).bold().padding(.leading).frame(maxWidth: .infinity, alignment: .leading)
+                    VStack(alignment : .center){
+                        ForEach(result){item in
+                            NavigationLink(
+                                destination: RestaurantDetail(restaurant: item)
+                            ){
+                                RestaurantItem(item : item)
+                            }
+                        }
+                    }.padding()
                     
                 }
-            }.padding(.leading).padding(.top)
+            }else{
+                Text("ไม่พบผลลัพธ์").foregroundColor(Color(red: 0.682, green: 0.702, blue: 0.745)).font(.callout).padding()
+            }
             
             
-            if(listSorted.count != 0){
-                Text("พบผลลัพธ์ทั้งหมด \(listSorted.count) รายการ").foregroundColor(Color(red: 0.00, green: 0.13, blue: 0.25)).bold().padding(.leading,20)
-            }
-            ScrollView(showsIndicators: false) {
-                ForEach(listSorted){item in
-                    NavigationLink(
-                        destination: RestaurantDetail(restaurant: item)
-                    ){
-                        RestaurantItem(item : item)
-                    }
-                }
-                
-            }
-            .padding()
         }.onAppear(perform: filterResult )
         .onChange(of:didSelectSort , perform: {value in
             switch value {
             case 0:
-                self.listSorted = listSorted.sorted {
+                self.result = result.sorted {
                     $0.distance < $1.distance
                 }
             case 1:
-                self.listSorted = listSorted.sorted {
+                self.result = result.sorted {
                     $0.price_level < $1.price_level
                 }
             case 2:
-                self.listSorted = listSorted.sorted {
-                    $0.price_level > $1.price_level
-                }
-            case 3:
-                self.listSorted = listSorted.sorted {
-                    $0.user_ratings_total > $1.user_ratings_total
+                self.result = result.sorted {
+                    $0.rating > $1.rating
                 }
             default:
-                self.listSorted = listSorted.sorted {
+                self.result = result.sorted {
                     $0.distance < $1.distance
                 }
             }
@@ -95,39 +101,62 @@ struct SearchResult: View {
     }
     
     func filterResult(){
+        if(text != ""){
+            self.result =  restaurantList.filter{(restaurant) -> Bool in restaurant.name.contains(text)}
+        }else{
+            self.result =  restaurantList
+        }
         
         if(!selectedCategory.isEmpty){
-            self.listSorted = restaurantList.filter { (restaurant) -> Bool in
-                if (restaurant.category.count > 2 ){
+            self.result = result.filter { (restaurant) -> Bool in
+                if (restaurant.category.count > 1 ){
                     return  selectedCategory.contains(restaurant.category[0]) || selectedCategory.contains(restaurant.category[1])
                 }
                 return selectedCategory.contains(restaurant.category[0])
             }
         }
         
-        if(selectedPrice != ""){
-            if(listSorted.isEmpty){
-                self.listSorted = restaurantList.filter{ restaurant -> Bool in
-                    return selectedPrice == restaurant.price_level || "ไม่ระบุราคา" == restaurant.price_level
-                }
+        if(!selectedPrice.isEmpty){
+            
+            print(selectedPrice)
+            self.result = result.filter{ restaurant -> Bool in
+                return selectedPrice.contains(restaurant.price_level)
             }
-            self.listSorted = listSorted.filter{ restaurant -> Bool in
-                return selectedPrice == restaurant.price_level || "ไม่ระบุราคา" == restaurant.price_level
-            }
+            
         }
         
         if(selectDistance != 0){
-            if(listSorted.isEmpty){
-                self.listSorted = restaurantList.filter{ restaurant -> Bool in
-                    return selectDistance >= restaurant.distance
-                }
-            }
-            self.listSorted = listSorted.filter{restaurant -> Bool in
+            self.result = result.filter{restaurant -> Bool in
                 return selectDistance >= restaurant.distance
             }
         }
         
-        self.listSorted = listSorted.sorted {
+        if(selectOption != ["","","","",""]){
+            if(selectOption[0] != ""){
+                self.result = result.filter{restaurant -> Bool in return restaurant.option[0]==selectOption[0]}
+            }
+                  
+            if(selectOption[1] != ""){
+                self.result = result.filter{restaurant -> Bool in return restaurant.option[1]==selectOption[1]}
+            }
+            
+            if(selectOption[2] != ""){
+                self.result = result.filter{restaurant -> Bool in return restaurant.option[2]==selectOption[2]}
+            }
+            
+            if(selectOption[3] != ""){
+                self.result = result.filter{restaurant -> Bool in return restaurant.option[3]==selectOption[3]}
+            }
+            
+        }
+        
+        if(!selectOpenNow){
+            self.result = result.filter{restaurant -> Bool in
+                return restaurant.open_now == true
+            }
+        }
+
+        self.result = result.sorted {
             $0.distance < $1.distance
         }
     }
--- PMU!/View/Search/SearchView.swift
+++ PMU!/View/Search/SearchView.swift
@@ -17,75 +17,71 @@ struct SearchView: View {
     @Environment(\.presentationMode) var presentationMode
     @State var searchInput: String = ""
     @State var didSelectCategory : [Int] = []
-    @State  var didSelectPrice : String = ""
+    @State var didSelectPrice : [String] = []
     @State var didSelectDistance : Double = 0
+    @State var didSelectOption : [String] = ["","","","",""]
+    @State var didSelectOpenNow : Bool = false
     @State var clickedSearch : Bool = false
-    @State var listSorted :[Restaurant] = []
+    @State var result :[Restaurant] = []
+    @State var commitSearch : Bool = false
     var restaurantList : [Restaurant]
     let screenWidth = UIScreen.main.bounds.width
     let screenHeight = UIScreen.main.bounds.height
     
     
     var body: some View {
-        VStack{
+        GeometryReader { geometry in
             
-            Group{
-                HStack{
-                    Button(action: {
-                        self.presentationMode.wrappedValue.dismiss()
-                    }){
-                        Image(systemName: "chevron.left").foregroundColor(Color(red: 0.00, green: 0.13, blue: 0.25)).font(.body).padding(.trailing)
-                    }
-                    TextField("      โปรดกรอกชื่อร้านอาหาร ...", text: $searchInput)
-                        
-                        .frame(width : screenWidth * 2 / 3 , height: 40)
-                        .background(Color(red: 0.898, green: 0.898, blue: 0.898, opacity: 0.22) )
-                        .cornerRadius(40)
-                        .foregroundColor(searchInput == "" ? Color(red: 0.682, green: 0.702, blue: 0.745) : Color(red: 0.00, green: 0.13, blue: 0.25))
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 40)
-                                .stroke(Color(red: 0.898, green: 0.898, blue: 0.898, opacity: 0.22), lineWidth: 2  )
-                        )
-                        .onTapGesture {
-                            self.clickedSearch = false
+            VStack{
+                Group{
+                    HStack{
+                        Button(action: {
+                            self.presentationMode.wrappedValue.dismiss()
+                        }){
+                            Image(systemName: "chevron.left").foregroundColor(Color(red: 0.00, green: 0.13, blue: 0.25)).font(.body).padding(.trailing)
                         }
+                        TextField("โปรดกรอกชื่อร้านอาหาร ...", text: $searchInput ,onCommit : { self.commitSearch = true })
+                            
+                            .frame(width : screenWidth * 2 / 3 , height: 40)
+                            .padding(.leading,20)
+                            .background(Color(red: 0.898, green: 0.898, blue: 0.898, opacity: 0.22) )
+                            .cornerRadius(40)
+                            .foregroundColor(searchInput == "" ? Color(red: 0.682, green: 0.702, blue: 0.745) : Color(red: 0.00, green: 0.13, blue: 0.25))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 40)
+                                    .stroke(Color(red: 0.898, green: 0.898, blue: 0.898, opacity: 0.22), lineWidth: 2  )
+                            )
+                            .onTapGesture {
+                                self.clickedSearch = false
+                                self.commitSearch = false
+                            }
+                        
+                        
+                        
+                        
+                        NavigationLink(
+                            destination: FilterView(Title : "การค้นหาขั้นสูง" , buttonTitle : "ค้นหา", didSelectCategory : $didSelectCategory , didSelectPrice : $didSelectPrice , didSelectDistance : $didSelectDistance , didSelectOption: $didSelectOption , didSelectOpenNow: $didSelectOpenNow, clickedSearch : $clickedSearch)
+                        ){
+                            Image(systemName: "slider.vertical.3").frame(width: screenWidth / 10 , height: screenWidth / 10) .cornerRadius(screenWidth / 20).font(.body).foregroundColor(Color(red: 0.00, green: 0.13, blue: 0.25))
+                                .overlay(RoundedRectangle(cornerRadius: screenWidth / 20)
+                                            .stroke(Color(red: 0.898, green: 0.898, blue: 0.898, opacity: 0.22), lineWidth: 2))
+                        }
+                    }.padding(.leading)
+                    .padding(.trailing)
+                    .padding(.top)
                     
-                    
-                    
-                    
-                    NavigationLink(
-                        destination: FilterView(didSelectCategory : $didSelectCategory , didSelectPrice : $didSelectPrice , didSelectDistance : $didSelectDistance , clickedSearch : $clickedSearch)
-                    ){
-                        Image("sliders").frame(width: screenWidth / 10 , height: screenWidth / 10) .cornerRadius(screenWidth / 20)
-                            .overlay(RoundedRectangle(cornerRadius: screenWidth / 20)
-                                        .stroke(Color(red: 0.898, green: 0.898, blue: 0.898, opacity: 0.22), lineWidth: 2))
-                    }
-                }.padding(.leading)
-                .padding(.trailing)
-                .padding(.top)
+                }
                 
-            }
-            
-            ScrollView{
-                SearchResult(listSorted : $listSorted , restaurantList: restaurantList , selectedCategory : $didSelectCategory , selectedPrice : $didSelectPrice , selectDistance : $didSelectDistance)
-                if(listSorted.isEmpty && clickedSearch){
-                    Text("ไม่พบผลลัพธ์").foregroundColor(Color(red: 0.682, green: 0.702, blue: 0.745)).font(.custom("Sukhumvit Set", size: 16)).padding()
+                if(clickedSearch || (searchInput != "" && commitSearch)){
+                    SearchResult(text : $searchInput , result: $result , restaurantList: restaurantList , selectedCategory : $didSelectCategory, selectedPrice : $didSelectPrice , selectDistance : $didSelectDistance ,selectOption: $didSelectOption , selectOpenNow : $didSelectOpenNow)
                 }
             }
-            
-            
         }
-        .font(.custom("Sukhumvit Set", size: 14))
+        .font(.subheadline)
         .navigationBarHidden(true)
-        .onChange(of: searchInput, perform: { value in
-            self.listSorted =  restaurantList.filter({ value == "" ? false : $0.name.contains(value) })
-        })
-        
+        .edgesIgnoringSafeArea(.bottom)
     }
     
-    func getSearchResult(){
-        
-    }
 }
 
 
