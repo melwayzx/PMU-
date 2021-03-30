054ceefc4ce4d17c0135e8737d7928e9d660597d
melwayzx <mewiiz.h@gmail.com>
2021-03-30 17:57:52 +0700

    Fix bug

--- PMU!/View/Filter/Items/SelectOptionItem.swift
+++ PMU!/View/Filter/Items/SelectOptionItem.swift
@@ -11,26 +11,28 @@ struct SelectOptionItem: View {
     
     var item : OptionChoice
     var didSelect : [String]
+    var didSelect2 : Bool
+    let screenWidth = UIScreen.main.bounds.width
     
     var body: some View {
-        HStack{
-            if(item.id == 0 ){
-                Image(systemName: item.logo)
-                    .resizable()
-                    .frame(width: 26, height: 26)
-                    .aspectRatio(1, contentMode: .fill)
-                    .foregroundColor(Color(red: 0.00, green: 0.13, blue: 0.25))
-            }else{
-                Image(item.logo)
-                    .resizable()
-                    .frame(width: 26, height: 26)
-                    .aspectRatio(1, contentMode: .fill)
-            }
-            Text(item.title).font(.callout).foregroundColor(didSelect.contains(item.title) ? Color(red: 0.00, green: 0.13, blue: 0.25): Color.gray  )
-            Spacer()
-            if(didSelect.contains(item.title)){
-            Image(systemName : "checkmark").foregroundColor(Color(red: 0.00, green: 0.13, blue: 0.25))
-            }
+        VStack{
+            HStack{
+                
+                if(item.title == "เปิดอยู่"){
+                    Text(item.title).font(.callout).foregroundColor( didSelect2 ? Color(red: 0.00, green: 0.13, blue: 0.25): Color.gray  )
+                    Spacer()
+                    if(didSelect2){
+                        Image(systemName : "checkmark").foregroundColor(Color(red: 0.00, green: 0.13, blue: 0.25) )
+                    }
+                }else{
+                    Text(item.title).font(.callout).foregroundColor(didSelect.contains(item.title) ? Color(red: 0.00, green: 0.13, blue: 0.25): Color.gray  )
+                    Spacer()
+                    if(didSelect.contains(item.title)){
+                        Image(systemName : "checkmark").foregroundColor(Color(red: 0.00, green: 0.13, blue: 0.25) )
+                    }
+                }
+            }.padding(.top,10)
+            //            Rectangle().fill(Color(red: 0.682, green: 0.702, blue: 0.745)).frame(width : screenWidth - 40 , height: 1)
         }
     }
 }
--- PMU!/View/Filter/Items/SelectPriceItem.swift
+++ PMU!/View/Filter/Items/SelectPriceItem.swift
@@ -20,12 +20,12 @@ struct SelectPriceItem: View {
         .padding(.leading,5)
         .padding(.trailing,5)
 //        .padding(.leading,10).padding(.trailing,10)
-        .background(didSelect.contains(item.subtitle) ? Color(red: 0.00, green: 0.13, blue: 0.25) : Color.white)
+        .background(didSelect.contains(item.title) ? Color(red: 0.00, green: 0.13, blue: 0.25) : Color.white)
         .cornerRadius(20)
-        .foregroundColor(didSelect.contains(item.subtitle)  ?  Color.white : Color(red: 0.682, green: 0.702, blue: 0.745) )
+        .foregroundColor(didSelect.contains(item.title)  ?  Color.white : Color(red: 0.682, green: 0.702, blue: 0.745) )
         .overlay(
             RoundedRectangle(cornerRadius: 20)
-                .stroke(didSelect.contains(item.subtitle)  ?  Color(red: 0.00, green: 0.13, blue: 0.25) : Color(red: 0.682, green: 0.702, blue: 0.745) , lineWidth: 1  )
+                .stroke(didSelect.contains(item.title)  ?  Color(red: 0.00, green: 0.13, blue: 0.25) : Color(red: 0.682, green: 0.702, blue: 0.745) , lineWidth: 1  )
         )
     }
 }
