49886239f36b8cf3722007857cf127d47b9b6d39
melwayzx <mewiiz.h@gmail.com>
2021-03-31 00:16:35 +0700

    Import Lottie

---
+++ PMU!/View/Random/LottieView.swift
@@ -0,0 +1,40 @@
+//
+//  LottieView.swift
+//  PMU!
+//
+//  Created by Melwayz's  on 30/3/21.
+//
+
+import SwiftUI
+import Lottie
+
+struct LottieView: UIViewRepresentable {
+    typealias UIViewType = UIView
+    var loopMode: LottieLoopMode = .loop
+    var name : String
+    
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
+        let view = UIView(frame: .zero)
+
+        let animationView = AnimationView()
+        let animation = Animation.named(name)
+        animationView.animation = animation
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = loopMode
+        animationView.play()
+        
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            animationView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            animationView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<LottieView>) { }
+}
